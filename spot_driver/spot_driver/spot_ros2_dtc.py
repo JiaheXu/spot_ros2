9d421fbed90b6cd5f05203bbdeeb9c314192725f
Jiahe Xu <xjh4438318846@gmail.com>
2025-02-05 18:40:34 -0500

    update

--- spot_driver/spot_driver/spot_ros2_dtc.py
+++ spot_driver/spot_driver/spot_ros2_dtc.py
@@ -174,8 +174,10 @@ from spot_driver.spot_ros2 import (
     SpotROS
 )
 
+from dtc_spot_msgs.srv import Anchor_3D
 
 from geometry_msgs.msg import PointStamped, TwistStamped, Twist
+from nav_msgs.msg import Path
 from std_msgs.msg import UInt8, Bool, String
 from sensor_msgs.msg import Joy
 # from airlab_msgs.msg import AIRLABModes
@@ -193,9 +195,6 @@ import numpy as np
 import open3d as o3d
 import transforms3d as t3d
 import math
-
-# for GPIO
-#import RPi.GPIO as GPIO
 
 class SpotROS_DTC(SpotROS):
 
@@ -220,77 +219,140 @@ class SpotROS_DTC(SpotROS):
         self._tf_listener = TFListenerWrapper(node)
         self._tf_listener.wait_for_a_tform_b(self._body_frame_name, self._vision_frame_name)
 
-
         # Pin Definitions
         self.estop_on = False
         self.estop_prev_value = False
         self.estop_input_pin = 22  # BOARD pin 22
-        
-        self.on_jetson = False
-        if( self.on_jetson ):
 
-            GPIO.setmode(GPIO.BOARD)  # BCM pin-numbering scheme from Raspberry Pi
-            GPIO.setup(self.estop_input_pin, GPIO.IN)  # set pin as an input pin
+        self.has_anchor = False
+        self.anchor = PointStamped()
+
+        # topic
+        self.tier2_estop_pub = self.create_subscriber(Bool, "/tier2_estop", self.estop_callback, 1)
+        
+        # service
+        # self.set_anchor_srv = self.create_service(Anchor_3D, 'set_anchor', self.set_anchor_callback)
+        # self.clear_anchor_srv = self.create_service(Trigger, 'clear_anchor', self.clear_anchor_callback)
+        
+        # action
+        # self.clear_anchor_srv = self.create_service(Trigger, 'clear_anchor', self.clear_anchor_callback)
+
+        self.startup()
 
         # output
-        self.tier2_estop_pub = self.create_publisher(Bool, "/tier2_estop", 1)
-
         # self.armed_status_pub = self.create_publisher(Bool, "status/arm", 1)
-        # self.spot_control_pub = self.create_publisher(Twist, "cmd_vel", 1)
-        self.count = 30
-        self.get_logger().info(COLOR_GREEN + "arm_control()!!!!!!!!!." + COLOR_END)
-        time.sleep(2)
-        self.arm_control()
-        time.sleep(2)
-        # self.timer = self.create_timer(0.1, self.status_timer_callback)
-        # self.look_at()
-        self.gaze()
-        # self.timer2 = self.create_timer(1, self.arm_control)
+        self.timer = self.create_timer(0.1, self.status_timer_callback)
+
 
     def echo(self, msg_str):
         self.get_logger().info(msg_str)
 
 
+    def startup(self): # some behaviors before we move the robot
+        while(True):
+            success, msg_info = self.spot_wrapper.spot_arm.gripper_open()
+            if(success):
+                break
+            self.get_logger().info(COLOR_GREEN + "Cannot open Griiper." + COLOR_END)
+
+    # def set_anchor_callback(self, request, response):
+    #     self.anchor = request.point
+    #     self.has_anchor = True
+    #     response.success = True
+    #     return response
+
+    # def clear_anchor_callback(self, request, response):
+    #     self.has_anchor = False
+    #     response.success = True
+    #     return response
+
     def status_timer_callback(self):
 
-        self.get_logger().info(COLOR_GREEN + "status_timer_callback()!!!!!!!!!." + COLOR_END)
-        if( self.on_jetson ):
-            value = GPIO.input(self.estop_input_pin)
-            if value == GPIO.HIGH:
-                self.estop_on = True
-            else:
-                self.estop_on = False
+        # self.get_logger().info(COLOR_GREEN + "status_timer_callback()!!!!!!!!!." + COLOR_END)
 
-        tier2_estop_msg = Bool()
-        tier2_estop_msg.data = self.estop_on
-        self.tier2_estop_pub.publish(tier2_estop_msg)
 
-        self.count -= 1
-        if(self.count == 0):
-            self.look_at( np.array([1., -1., 0. ] ))
+        # tier2_estop_msg = Bool()
+        # tier2_estop_msg.data = self.estop_on
+        # self.tier2_estop_pub.publish(tier2_estop_msg)
+
         # arm_msg = Bool()
         # arm_msg.data = self.arm_status
         # self.armed_status_pub.publish(arm_msg)
 
         return
 
-    def arm_control(self):
-        # self.spot_wrapper.time_sync.wait_for_sync()
 
-        assert self.spot_wrapper.has_arm(), 'Robot requires an arm to run this example.'
-        
-        self.get_logger().info(COLOR_GREEN + "has_arm." + COLOR_END)
-        # Verify the robot is not estopped and that an external application has registered and holds
-        # an estop endpoint.
-        assert not self.spot_wrapper.is_estopped(), 'Robot is estopped. Please use an external E-Stop client, ' \
-                                        'such as the estop SDK example, to configure E-Stop.'
-        
-        self.get_logger().info(COLOR_GREEN + "not estopped." + COLOR_END)
-        _, __ = self.spot_wrapper.spot_arm.gripper_open()
+    def estop_callback(self, msg):
+        self.estop_on = msg.data
 
-        # unstow = RobotCommandBuilder.arm_ready_command()
-        # unstow_command_id = self.command_client.robot_command(unstow)
-        self.spot_wrapper.spot_arm.arm_unstow()
+    # def follow_traj(self, path_msg):
+
+    #     # arm traj
+    #     for ii in range(_N_POINTS + 1):
+    #         # Get coordinates relative to the robot's body
+    #         y = (_L_ROBOT_SQUARE / 2) - _L_ARM_CIRCLE * (np.cos(2 * ii * math.pi / _N_POINTS))
+    #         z = _VERTICAL_SHIFT + _L_ARM_CIRCLE * (np.sin(2 * ii * math.pi / _N_POINTS))
+
+    #         # Using the transform we got earlier, transform the points into the vision frame
+    #         x_ewrt_vision, y_ewrt_vision, z_ewrt_vision = vision_T_body.transform_point(x, y, z)
+
+    #         # Add a new point to the robot command's arm cartesian command se3 trajectory
+    #         # This will be an se3 trajectory point
+    #         point = command.synchronized_command.arm_command.arm_cartesian_command.pose_trajectory_in_task.points.add(
+    #         )
+
+    #         # Populate this point with the desired position, rotation, and duration information
+    #         point.pose.position.x = x_ewrt_vision
+    #         point.pose.position.y = y_ewrt_vision
+    #         point.pose.position.z = z_ewrt_vision
+
+    #         point.pose.rotation.x = vision_T_body.rot.x
+    #         point.pose.rotation.y = vision_T_body.rot.y
+    #         point.pose.rotation.z = vision_T_body.rot.z
+    #         point.pose.rotation.w = vision_T_body.rot.w
+
+    #         traj_time = (ii + 1) * seconds_arm
+    #         duration = seconds_to_duration(traj_time)
+    #         point.time_since_reference.CopyFrom(duration)
+
+
+    #     # mobile traj
+    #     for ii in range(x_vals.size):
+    #         # Pull the point in the square relative to the robot and scale according to param
+    #         x = _L_ROBOT_SQUARE * x_vals[ii]
+    #         y = _L_ROBOT_SQUARE * y_vals[ii]
+
+    #         # Transform desired position into vision frame
+    #         x_ewrt_vision, y_ewrt_vision, z_ewrt_vision = vision_T_body.transform_point(x, y, 0)
+
+    #         # Add a new point to the robot command's arm cartesian command se3 trajectory
+    #         # This will be an se2 trajectory point
+    #         point = command.synchronized_command.mobility_command.se2_trajectory_request.trajectory.points.add(
+    #         )
+
+    #         # Populate this point with the desired position, angle, and duration information
+    #         point.pose.position.x = x_ewrt_vision
+    #         point.pose.position.y = y_ewrt_vision
+
+    #         point.pose.angle = vision_T_body.rot.to_yaw()
+
+    #         traj_time = (ii + 1) * seconds_body
+    #         duration = seconds_to_duration(traj_time)
+    #         point.time_since_reference.CopyFrom(duration)
+        
+    #     # set the frame for the body trajectory
+    #     command.synchronized_command.mobility_command.se2_trajectory_request.se2_frame_name = frame_name
+    #     mobility_params = spot_command_pb2.MobilityParams(vel_limit=speed_limit)
+    #     command.synchronized_command.mobility_command.params.CopyFrom(
+    #         RobotCommandBuilder._to_any(mobility_params))
+    #     # Send the command using the command client
+    #     # The SE2TrajectoryRequest requires an end_time, which is set
+    #     # during the command client call
+    #     robot.logger.info('Sending arm and body trajectory commands.')
+    #     command_client.robot_command(command, end_time_secs=time.time() + _SECONDS_FULL)
+    #     time.sleep(_SECONDS_FULL + 2)
+
+
 
     def get_lookat_position(self, position, target):
         # we only rotate z and y axis
@@ -337,6 +399,7 @@ class SpotROS_DTC(SpotROS):
         pose_msg.header.frame_id="body"
         
         self.arm_pose_cmd_callback(pose_msg)
+
         # move to goal 
     def gaze(self):
         
